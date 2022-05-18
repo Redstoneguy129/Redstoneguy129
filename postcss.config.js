fb7ca34bd7604c0084c121808bc299de73b5b532
Redstoneguy129 <public@cameronwhyte.me>
2022-05-18 21:55:20 +0000

    tailwind now works

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
-    ],
+    plugins: {
+        tailwindcss: {},
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    },
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { h, render } from "preact";
 import App from "./App";
+import './global.css'
 
 render(<App/>, document.body);
